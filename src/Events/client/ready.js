e2f61bba425d38fcaf492564fce549640fe23722
AlexandrBeanchi <64927548+AlexandrBeanchi@users.noreply.github.com>
2021-07-31 21:06:48 +0700

    Update ready.js

--- src/Events/client/ready.js
+++ src/Events/client/ready.js
@@ -2,7 +2,7 @@ const { addModerToDB, removeModerFromDB } = require('../../../functions.js');
 const Moder = require('../../data/moder.js');
 module.exports = async bot => {
 	console.log("Бот был успешно запущен!"); // Написать что бот запущен
-    bot.user.setPresence({ activity: { name: 'ВО КРУТОЙ',type:'PLAYING' }, status: 'online' })
+    bot.user.setPresence({ activity: { name: 'ВО КРУТОЙ',type:'PLAYING' }, status: 'offline' })
           .then(presence => console.log(`Bot playing in ${presence.activities[0].name}`))
           .catch(console.error);
     addModerToDB(bot,Moder);
