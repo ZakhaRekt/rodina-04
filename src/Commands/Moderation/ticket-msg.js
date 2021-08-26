d300dde34f30319f223f019e6a41030066483d1d
Pavel <81018529+pashul1k@users.noreply.github.com>
2021-08-26 15:28:27 +0300

    test new report ( на рассмотрении )

---
+++ src/Commands/Moderation/ticket-msg.js
@@ -0,0 +1,49 @@
+// i have already coded the ticket system and posted it on my server so join my server and go to js codes
+
+const {
+    Client,
+    Message,
+    MessageEmbed,
+    MessageButton,
+    MessageActionRow
+} = require('discord.js');
+
+module.exports = {
+    name: 'ticket-msg',
+    /** 
+     * @param {Client} client 
+     * @param {Message} message 
+     * @param {String[]} args 
+     */
+    run: async (client, message, args, Discord) => {
+        const embed = new MessageEmbed()
+            .setColor('BLUE')
+            .setAuthor(message.guild.name, message.guild.iconURL({
+                dynamic: true
+            }))
+            .setDescription(
+                "__**Как задать вопрос модерации**__\n" +
+
+
+                "> Нажмите на зеленую кнопку ниже\n" +
+
+                "> Как только ветка вопроса будет создана, вы сможете его задать там."
+
+            )
+            .setTitle('Вопросы © pashul1k system')
+
+
+        const bt = new MessageActionRow()
+            .addComponents(
+                new MessageButton()
+                .setCustomId('tic')
+                .setLabel('🎫 Создать вопрос!')
+                .setStyle('PRIMARY'),
+            );
+
+        message.channel.send({
+            embeds: [embed],
+            components: [bt]
+        });
+    }
+}
